279535f059261dd9b86718d454978f21ffdf5ad1
Nguyễn Thị Hà <hantctk42@gmail.com>
2022-09-27 16:36:31 +0700

    Connect mongodb 🍟🍰🍟

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import posts from "./routers/posts.js";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.port || 5000;
+
+const URI =
+  "mongodb+srv://admin:AgtRolOZZkTb7hIT@cluster0.yozx7ee.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
@@ -12,8 +16,17 @@ app.use(cors());
 
 app.use("/posts", posts);
 
-// http://localhost:5000
+// Kết nối đến mongodb
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to DB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// http://localhost:5000
